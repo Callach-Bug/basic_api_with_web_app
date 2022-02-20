55248fe5ebca55709757ca6f93267707e5872a7a
james <originaltitans@yahoo.co.uk>
2022-02-20 01:27:41 +0000

    Made sure the filewriting prints to file

--- REST/src/Testing.java
+++ REST/src/Testing.java
@@ -2,18 +2,21 @@ import java.io.*;
 
 public class Testing {
     public static void main(String[] args) {
-        new Testing().Filez();
+            Testing a = new Testing();
+            a.Filez();
     }
     public void Filez(){
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("test.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("retry.txt"));
             writer.write("Hi Callum");
+            writer.close();
         }catch (Exception e){
             e.printStackTrace();
         }
         try{
-            BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("retry.txt"));
             reader.readLine();
+            reader.close();
         }catch (Exception e){
             e.printStackTrace();
         }
